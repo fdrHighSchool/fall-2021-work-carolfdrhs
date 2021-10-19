703ecad9fd96be1080ef4e4fe3870244acf21aef
Carol Chancharastong <carolc4@nycstudents.net>
2021-10-19 08:13:54 -0400

    cutting code

--- random number game/numbergame.java
+++ random number game/numbergame.java
@@ -1,52 +1,44 @@
-import java.util.Scanner;
+import java.util.*;
 import java.util.Random;
 
 
 class numbergame {
+  static Scanner ask = new Scanner(System.in);
+
   public static void main(String[] args) {
     //ask user for their name
-    Scanner askName = new Scanner(System.in);
     System.out.println("What is your name?");
-    String userName = askName.nextLine();
+    String userName = ask.nextLine();
 
-    String restart = "0";
-    int restartCheck = 1;
-    do{
+    do{//restart loop
       //ask user for difficulty
-      Scanner askMode = new Scanner(System.in);
+
       System.out.println("Hello " + userName + ", what difficulty would you like when playing this number guessing game? \neasy \nmedium \nhard \ncustom");
-      String difficulty = askMode.nextLine();
+      String difficulty = ask.nextLine();
       difficulty = difficulty.toLowerCase(); //in case of capital letters
 
-      int easy = difficulty.compareTo("easy");
-      int medium = difficulty.compareTo("medium");
-      int hard = difficulty.compareTo("hard");
-      int custom = difficulty.compareTo("custom");
+
     //print ending statement when user wins
-      if (easy == 0) {
+      if (difficulty.equals("easy")) {
         System.out.println("Correct "+ userName + ", it took " + gameStart(1,10) + " tries");
       }
-      if (medium == 0) {
+      if (difficulty.equals("medium")) {
         System.out.println("Correct "+ userName + ", it took " + gameStart(1,100) + " tries");
       }
-      if (hard == 0) {
+      if (difficulty.equals("hard")) {
         System.out.println("Correct "+ userName + ", it took " + gameStart(1,1000) + " tries");
       }
-      if (custom == 0) {
+      if (difficulty.equals("custom")) {
         System.out.println("choose a number to be the maximum");
-        int customPick = askMode.nextInt();
+        int customPick = ask.nextInt();
         System.out.println("Correct "+ userName + ", it took " + gameStart(1,customPick) + " tries");
       }
+      if (difficulty.equals("quit")){
+        break;
+      }
 
-
-    //ask if wanna play again
-      Scanner askRestart = new Scanner(System.in);
-      System.out.println("would you like to play again? (yes or no)");
-      restart = askRestart.nextLine();
-      restart = restart.toLowerCase();
-      restartCheck = restart.compareTo("no");
     }
-    while (restartCheck != 0);
+    while ();//in progress
   }
 
 
@@ -58,9 +50,8 @@ class numbergame {
     int number = random.nextInt(max) + 1;
 
     //ask the user for their number guess thing
-    Scanner askGuess = new Scanner(System.in);
     System.out.println("Guess a number from 1-"+ max);
-    int guess = askGuess.nextInt();
+    int guess = ask.nextInt();
 
 
     //setting up variables for while statement
@@ -72,13 +63,13 @@ class numbergame {
       if (guess > number) {
         maxNum = guess - 1;
         System.out.println("Wrong too high! Pick another number from " + minNum + "-" + maxNum);
-        guess = askGuess.nextInt();
+        guess = ask.nextInt();
 
       }
       else if (guess < number) {
         minNum = guess + 1;
         System.out.println("Wrong too low! Pick another number from " + minNum + "-" + maxNum);
-        guess = askGuess.nextInt();
+        guess = ask.nextInt();
 
       }
       numGuess++;
