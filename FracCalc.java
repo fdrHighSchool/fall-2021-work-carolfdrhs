5383f0bb63651ad4863e8500d82f30c3ec9d2b03
Carol Chancharastong <carolc4@nycstudents.net>
2021-11-08 08:47:34 -0500

    frac calc start

---
+++ FracCalc.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class FracCalc{
+  public static void main(String[] args){
+    Scanner ask = new Scanner(System.in);
+    System.out.println("input your equation:");
+    String equation = ask.nextLine();
+
+
+
+  }
+
+
+
+
+
+
+    public static int findNum(String equation, int term){
+      if (term == 1){
+        System.out.println(equation.substring(0,); //
+        return()
+      }
+      else{
+
+      }
+
+
+    }
+    public static int findDen(String){
+
+
+
+    }
+
+
+
+
+
+
+}
