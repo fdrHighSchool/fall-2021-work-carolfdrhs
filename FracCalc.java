d3e5cf6dba50aadf3ff622ea3760f5b84efb0d35
Carol Chancharastong <carolc4@nycstudents.net>
2021-11-09 08:46:20 -0500

    find numerator of both numbers

--- FracCalc.java
+++ FracCalc.java
@@ -5,9 +5,7 @@ public class FracCalc{
     Scanner ask = new Scanner(System.in);
     System.out.println("input your equation:");
     String equation = ask.nextLine();
-
-
-
+    findNum(equation,2);
   }
 
 
@@ -15,22 +13,36 @@ public class FracCalc{
 
 
 
-    public static int findNum(String equation, int term){
+    public static void findNum(String equation, int term){
       if (term == 1){
-        System.out.println(equation.substring(0,); //
-        return()
+
+        if (equation.indexOf("/") > equation.indexOf(" ")){ //if there isnt a / in the first term indexOf =-1
+          System.out.println(equation.substring(0,equation.indexOf(" ")));//stores the whole number
+        }
+        else{
+          System.out.println(equation.substring(0,equation.indexOf("/")));//stores the numerator
+
+        }
+
       }
+
       else{
+        String term2 = equation.substring(equation.lastIndexOf(" "));
+        if (term2.indexOf("/") == -1){ //if there isnt a / in the second term
+          System.out.println(equation.substring(equation.lastIndexOf(" ")+1));//takes the whole number
 
+        }
+        else{
+          System.out.println(equation.substring(equation.lastIndexOf(" ")+1,equation.lastIndexOf("/")));
+
+
+        }
+        //System.out.println(equation.substring(equation.indexOf(" ")+3)); //term 2
       }
 
 
     }
-    public static int findDen(String){
 
-
-
-    }
 
 
 
