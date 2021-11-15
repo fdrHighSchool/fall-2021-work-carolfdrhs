fa4f840d518348eedf390e9b5bf83e380a123c97
Carol Chancharastong <carolc4@nycstudents.net>
2021-11-15 08:48:01 -0500

    denominator

--- calc.java
+++ calc.java
@@ -11,9 +11,18 @@ public class calc{
 
     System.out.println(operand1);
     System.out.println(operand2);
+    System.out.println();
 
     System.out.println(findNum(operand1));
     System.out.println(findNum(operand2));
+    System.out.println();
+
+    System.out.println(findDen(operand1));
+    System.out.println(findDen(operand2));
+    System.out.println();
+
+    System.out.println(findWhole(operand1));
+    System.out.println(findWhole(operand2));
 
   }
   public static int findNum(String operand){
@@ -30,12 +39,17 @@ public class calc{
 
 
   public static int findDen(String operand){
-    if(operand.indexOf("_") != -1){
-        return Integer.parseInt(operand.substring(operand.indexOf("/")+1));
-      }
+    if (operand.indexOf("/") == -1){//if there isnt a slash
+      return Integer.parseInt(operand);//take whole number
+
+    }
+    else{//if theres a slash
+      return Integer.parseInt(operand.substring("/")+1); //take denominator
     }
 
-    
+    }
+
+
     public static int findWhole(String operand){
       return Integer.parseInt(operand.substring(0, operand.indexOf("_")));
 
