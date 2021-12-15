059df31b47c4868bd75f11ff86bf13a7a62d6342
Carol Chan <carolc4@nycstudents.net>
2021-12-15 06:36:52 -0500

    range

--- 06-Centralmeasure/CentralMeasure.java
+++ 06-Centralmeasure/CentralMeasure.java
@@ -11,50 +11,87 @@ public class CentralMeasure{
     //print list check
     System.out.println(Arrays.toString(randomArray));
 
+    System.out.println(range(randomArray));
+
   }//end main
 
-    public static int mode(int[] arr){
-      //find the max value of the list (check the randomArray list)
-      int max = findMax(arr);
+  public static int mode(int[] arr){
+    //find the max value of the list (check the randomArray list)
+    int max = findMax(arr);
 
-      //make an array of that size (fill all values with 0)
-      int[] frequency = new int[max+1];
-      for(int i = 0; i< frequency.length; i++){
-        frequency[i] = 0;
-      }
-      //System.out.println(Arrays.toString(frequency));
+    //make an array of that size (fill all values with 0)
+    int[] frequency = new int[max+1];
 
-      //tally the frequency of occurance of each unique number
-      for(int i = 0; i < arr.length; i++){
-         frequency[arr[i]1]++;
-      }
-      //System.out.println(Arrays.toString(frequency));
+    //System.out.println(Arrays.toString(frequency));
 
-      //find which has the largest frequency
-      //findMax()
-      return 0;
+    //tally the frequency of occurance of each unique number
+    for(int i = 0; i < arr.length; i++){
+       frequency[arr[i]]++;
+    }
+    //System.out.println(Arrays.toString(frequency));
+
+    //find which has the largest frequency
+    //findMax()
+    return 0;
 
 
 
     }//end mode
+  public static double average(int[] arr) {
+   // declare accumulator variable
+   double total = 0.0;
+
+   // add up all of the data
+   for(int i = 0; i < arr.length; i++) {
+     total += arr[i];
+   }
+
+   // divide total by number of data values
+   return total / arr.length;
+  } // end average method
+
+
+  public static int range(int[] arr){
+    //find the max and min; subtract
+    return findMax(arr)-findMin(arr);
 
 
 
-    public static int findMax(int[] arr){
-    int max = arr[0];
-    int place = 0;
-    for(int i = 0; i < arr.length; i++){
-      if(max < arr[i]){
-        max = arr[i];
-        place = i;
-      }
-      //what about if they have the same frequency?
+  }
+
+
+  public static int findMax(int[] arr){
+  int max = arr[0];
+  int place = 0;
+  for(int i = 0; i < arr.length; i++){
+    if(max < arr[i]){
+      max = arr[i];
+      place = i;
     }
-    System.out.println(max);
+    //what about if they have the same frequency?
+  }
+  //System.out.println(max);
 
-    //return(place+1);
-    return max;
-  }//end findMax
+  //return(place+1); //index return
+  return max;
+}//end findMax
+
+
+public static int findMin(int[] arr){
+int min = arr[0];
+int place = 0;
+for(int i = 0; i < arr.length; i++){
+  if(min > arr[i]){
+    min = arr[i];
+    place = i;
+  }
+  //what about if they have the same frequency?
+}
+//System.out.println(min);
+
+//return(place+1); //index return
+return min;
+}//end findMin
 
 
 
